1cedb05c77020567c2d2033b957dc8a4b2179231
Youssef Nabeh <115867801+yousseffe@users.noreply.github.com>
2024-07-18 16:23:02 +0300

    Deep copy Vs Shallow copy [ array of value type ]

--- Program.cs
+++ Program.cs
@@ -34,7 +34,42 @@ namespace Assigment_OOP_4
                 IFlyable airplane3 = new Airplane();
 
                 airplane3.Forward();
-                airplane2.Forward();*/ 
+                airplane2.Forward();*/
+            #endregion
+
+            #region Shallow Copy VS Deep Copy
+/*
+            int[] Arr01 = { 1 , 2 , 3 };
+            int[] Arr02 = { 4 , 5 , 6 };
+            Console.WriteLine($"HashCode Arr01 = {Arr01.GetHashCode}");
+            Console.WriteLine($"HashCode Arr02 = {Arr02.GetHashCode}");
+            #region Shallow copy
+            *//*//shallow copy
+                Arr02 = Arr01;
+                // this object {1,2,3} has 2 refrences []
+                // this object {4,5,6} unreachable object
+                Console.WriteLine("After shallow copy");
+
+                Console.WriteLine($"HashCode Arr01 = {Arr01.GetHashCode}");
+                Console.WriteLine($"HashCode Arr02 = {Arr02.GetHashCode}");
+
+                Arr02[0] = 100;
+                Console.WriteLine(Arr01[0]);*//*
+            #endregion
+
+            #region Deep copy
+            *//*Arr02 = (int[])Arr01.Clone();
+
+            Console.WriteLine("After Deep Copy");
+            Console.WriteLine($"HashCode Arr01 = {Arr01.GetHashCode}");
+            Console.WriteLine($"HashCode Arr02 = {Arr02.GetHashCode}");
+
+            Arr02[0] = 100;
+            Console.WriteLine(Arr01[0]);
+            Console.WriteLine($"HashCode Arr01 = {Arr01.GetHashCode}");
+            Console.WriteLine($"HashCode Arr02 = {Arr02.GetHashCode}");*//*
+            #endregion
+*/
             #endregion
         
             
