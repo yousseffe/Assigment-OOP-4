a18a1035f1eb8ac3a9f758f816da6f097cc37d20
Youssef Nabeh <115867801+yousseffe@users.noreply.github.com>
2024-07-18 15:46:32 +0300

    Interfac - Example 2

---
+++ Interface/ISeries.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assigment_OOP_4.Interface
+{
+    internal interface ISeries
+    {
+        // 1. signture for property
+
+        public int Current { get; set; }
+
+        // 2. signture for method
+
+        public void getNext();
+
+        public void Reset()
+        {
+            Current = 0;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,27 @@
-﻿namespace Assigment_OOP_4
+﻿using Assigment_OOP_4.Interface;
+
+namespace Assigment_OOP_4
 {
     internal class Program
     {
+        public static void Print10NumbersFromSeries(ISeries series)
+        {
+            if (series == null)
+                return;
+            for (int i = 0; i < 10; i++) {
+                Console.Write($"{series.Current}\t");
+                series.getNext();
+            }
+            series.Reset();
+            Console.WriteLine();
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            ISeries seriesByTwo = new SeriesByTwo();
+            ISeries seriesByThree = new SeriesByThree();
+            ISeries seriesByFour = new SeriesByFour();
+
+            Print10NumbersFromSeries(seriesByFour);
         }
     }
 }
